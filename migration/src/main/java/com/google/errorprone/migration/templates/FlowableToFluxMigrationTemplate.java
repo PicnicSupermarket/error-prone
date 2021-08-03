2900eb5ffa5f63526e8793fce540a98a4b4c5367
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-08-03 16:16:37 +0200

    Add new migrations and update test for MigrationExtractionTest

---
+++ migration/src/main/java/com/google/errorprone/migration/templates/FlowableToFluxMigrationTemplate.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.migration.templates;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import com.google.errorprone.refaster.annotation.MigrationTemplate;
+import io.reactivex.Flowable;
+import reactor.adapter.rxjava.RxJava2Adapter;
+import reactor.core.publisher.Flux;
+
+public final class FlowableToFluxMigrationTemplate {
+  private FlowableToFluxMigrationTemplate() {}
+
+  static final class FlowableToFlux {
+    @MigrationTemplate(value = false)
+    static final class MigrateFlowableToFlux<T> {
+      @BeforeTemplate
+      Flowable<T> before(Flowable<T> flowable) {
+        return flowable;
+      }
+
+      @AfterTemplate
+      Flux<T> after(Flowable<T> flowable) {
+        return flowable.as(RxJava2Adapter::flowableToFlux);
+      }
+    }
+
+    @MigrationTemplate(value = true)
+    static final class MigrateFluxToFlowable<T> {
+      @BeforeTemplate
+      Flux<T> before(Flux<T> flux) {
+        return flux;
+      }
+
+      @AfterTemplate
+      Flowable<T> after(Flux<T> flux) {
+        return flux.as(RxJava2Adapter::fluxToFlowable);
+      }
+    }
+  }
+}
---
+++ migration/src/main/java/com/google/errorprone/migration/templates/MaybeNumberToMonoNumberMigrationTemplate.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.migration.templates;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import com.google.errorprone.refaster.annotation.MigrationTemplate;
+import io.reactivex.Maybe;
+import reactor.adapter.rxjava.RxJava2Adapter;
+import reactor.core.publisher.Mono;
+
+public final class MaybeNumberToMonoNumberMigrationTemplate {
+  private MaybeNumberToMonoNumberMigrationTemplate() {}
+
+  static final class MaybeNumberToMonoNumber {
+    @MigrationTemplate(value = false)
+    static final class MigrateMaybeNumberToMonoNumber<T extends Number> {
+      @BeforeTemplate
+      Maybe<T> before(Maybe<T> maybe) {
+        return maybe;
+      }
+
+      @AfterTemplate
+      Mono<T> after(Maybe<T> maybe) {
+        return maybe.as(RxJava2Adapter::maybeToMono);
+      }
+    }
+
+    @MigrationTemplate(value = true)
+    static final class MigrateMonoNumberToMaybeNumber<T> {
+      @BeforeTemplate
+      Mono<T> before(Mono<T> mono) {
+        return mono;
+      }
+
+      @AfterTemplate
+      Maybe<T> after(Mono<T> mono) {
+        return mono.as(RxJava2Adapter::monoToMaybe);
+      }
+    }
+  }
+}
