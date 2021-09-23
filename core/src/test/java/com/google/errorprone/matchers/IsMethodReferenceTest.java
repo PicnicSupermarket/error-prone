1aba00d42cdc6aaed88c68dcc330fd802f05acd1
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-09-23 13:02:34 +0200

    Add test for IsMethodReference in advance and add lambda test cases to InlinerTest

---
+++ core/src/test/java/com/google/errorprone/matchers/IsMethodReferenceTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2021 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.matchers;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.scanner.Scanner;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.ReturnTree;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class IsMethodReferenceTest extends CompilerBasedAbstractTest {
+  private final List<ScannerTest> tests = new ArrayList<>();
+
+  //  private List<String> foo() {
+  //    return ImmutableList.of(1, 2).stream().map(String::valueOf).collect(Collectors.toList());
+  //  }
+  //
+  //  private List<String> bar() {
+  //    return ImmutableList.of(1, 2).stream().map(e ->
+  // String.valueOf(e)).collect(Collectors.toList());
+  //  }
+
+  @After
+  public void tearDown() {
+    for (ScannerTest test : tests) {
+      test.assertDone();
+    }
+  }
+
+  @Test
+  public void positive() {
+    writeFile(
+        "A.java",
+        "import com.google.common.collect.ImmutableList;",
+        "import java.util.List",
+        "public class A {",
+        "  public List<String> foo() {",
+        "    return ImmutableList.of(1, 2).stream().map(String::valueOf).collect(Collectors.toList());",
+        "  }",
+        "}");
+
+    assertCompiles(isFunctionMethodReference(/* shouldMatch= */ true, new IsMethodReference()));
+  }
+
+  @Test
+  public void negative() {
+    writeFile(
+        "A.java",
+        "import com.google.common.collect.ImmutableList;",
+        "import java.util.List",
+        "public class A {",
+        "  public List<String> bar() {",
+        "    return ImmutableList.of(1, 2).stream().map(e -> String.valueOf(e)).collect(Collectors.toList());",
+        "  }",
+        "}");
+
+    assertCompiles(isFunctionMethodReference(/* shouldMatch= */ false, new IsMethodReference()));
+  }
+
+  private abstract static class ScannerTest extends Scanner {
+    abstract void assertDone();
+  }
+
+  private Scanner isFunctionMethodReference(
+      final boolean shouldMatch, final Matcher<ExpressionTree> toMatch) {
+    ScannerTest test =
+        new ScannerTest() {
+          private boolean matched = false;
+
+          @Override
+          public Void visitReturn(ReturnTree node, VisitorState visitorState) {
+            visitorState = visitorState.withPath(getCurrentPath());
+            if (toMatch.matches(node.getExpression(), visitorState)) {
+              matched = true;
+            }
+            return super.visitReturn(node, visitorState);
+          }
+
+          @Override
+          public void assertDone() {
+            assertThat(shouldMatch).isEqualTo(matched);
+          }
+        };
+    tests.add(test);
+    return test;
+  }
+}
