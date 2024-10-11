49a3dbe9a427143247ba400920e54c427f6bc381
ghm <ghm@google.com>
2024-10-11 07:18:45 -0700

    Add a check discouraging \s, unless it's a sequence of \s terminating a line.
    What's your view on this being an error? On one hand, it seems sensible to make things errors now, given there's zero adoption, and relax later as needed.
    
    PiperOrigin-RevId: 684828665
    (cherry picked from commit 78729ffb01d6b022832994066425c158ee7a988f)

--- core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
+++ core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
@@ -234,6 +234,7 @@ import com.google.errorprone.bugpatterns.MathRoundIntLong;
 import com.google.errorprone.bugpatterns.MemberName;
 import com.google.errorprone.bugpatterns.MemoizeConstantVisitorStateLookups;
 import com.google.errorprone.bugpatterns.MethodCanBeStatic;
+import com.google.errorprone.bugpatterns.MisleadingEscapedSpace;
 import com.google.errorprone.bugpatterns.MissingBraces;
 import com.google.errorprone.bugpatterns.MissingCasesInEnumSwitch;
 import com.google.errorprone.bugpatterns.MissingDefault;
@@ -765,6 +766,7 @@ public class BuiltInCheckerSuppliers {
           LossyPrimitiveCompare.class,
           MathRoundIntLong.class,
           MislabeledAndroidString.class,
+          MisleadingEscapedSpace.class,
           MisplacedScopeAnnotations.class,
           MissingSuperCall.class,
           MissingTestCall.class,
